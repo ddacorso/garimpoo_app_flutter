e9e7169f39b84f248898b3c98c3452a62ddd43df
user219813 <ddacorso@gmail.com>
2022-05-13 13:56:19 +0200

    fixed ios build

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by user219813 on 5/6/22.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
